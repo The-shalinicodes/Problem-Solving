c1e3364ff278823dcfff0b9ed3f1fe7d5613dae8
Shalu <shlokashalini@gmail.com>
2026-01-22 13:57:44 +0530

    Added levelOrder Solution

---
+++ Java/levelOrder.java
@@ -0,0 +1,21 @@
+class Solution {
+  public List<List<Integer>> levelOrder(TreeNode root) {
+    // your code goes here
+    List<List<Integer>> ans = new ArrayList<>();
+    if (root == null) return ans;
+    Queue<TreeNode> q = new LinkedList<>();
+    q.add(root);
+    while (!q.isEmpty()) {
+      int size = q.size();
+      List<Integer> level = new ArrayList<>();
+      for (int i = 0; i < size; i++) {
+        TreeNode node = q.poll();
+        level.add(node.data);
+        if (node.left != null) q.add(node.left);
+        if (node.right != null) q.add(node.right);
+      }
+      ans.add(level);
+    }
+    return ans;
+  }
+}
