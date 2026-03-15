f480fd3417badc33e4a2716bfc22593ce1d03817
Shalu <shlokashalini@gmail.com>
2026-03-15 20:28:32 +0530

    Added hIndex Solution

--- Java/hIndex.java
+++ Java/hIndex.java
@@ -1,13 +1,12 @@
 class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
+        int count=0;
         int n=citations.length;
         for(int i=0;i<n;i++){
-            int h=n-i;
-            if(citations[i]>=h){
-                return h;
+            if(citations[i]>=n-i){
+                count++;
             }
         }
-        return 0;
+        return count;
     }
 }
